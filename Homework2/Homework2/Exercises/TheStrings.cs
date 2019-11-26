30654adad1aeb478a374a078e1f5ff10ad09bd64
Andrei Botezatu <andrei_botezatu_90@yahoo.com>
2019-11-26 21:11:18 +0200

    initial commit

---
+++ Homework2/Homework2/Exercises/TheStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework2.Exercises
+{
+    class TheStrings 
+    {
+        //Write a method that to remove the nth index character from a nonempty string.
+        public void Problem1()
+        {
+            string s1 = "string and arrays";                     
+            Console.WriteLine("first " + s1.Remove(7,10));
+
+            
+
+
+
+        }
+
+
+
+    }
+
+}
---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Homework2.Exercises;
+
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TheStrings home1 = new TheStrings();
+            home1.Problem1();
+
+
+
+
+
+        }
+    }
+}
